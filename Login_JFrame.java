e11c59db16f835bf191c5ecfffff49941e2fafca
doantx <92896474+doantx@users.noreply.github.com>
2021-10-20 19:44:24 -0400

    Update Login_JFrame.java

--- Login_JFrame.java
+++ Login_JFrame.java
@@ -1,15 +1,9 @@
 
 import javax.swing.JFrame;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author Seuz
+ * @author S
  */
 public class Login_JFrame extends javax.swing.JFrame {
 
