11eb5f57868f29e989c3bedb7a95b21c1c29148f
doantx <92896474+doantx@users.noreply.github.com>
2021-11-11 13:41:58 -0500

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,77 +1,70 @@
 import java.util.*;
 
 public class Application {
+    public String appName, description, organization, platform, version, linkToStore; 
+    public float price; 
+    public ArrayList<String> comments; 
 
-	public String appName, description, platform, version, linkToStore; 
-	public float price; 
-	public ArrayList<String> comments; 
-	 
-	/*	
-	 * Default constructor that initializes the name, description, platform, 
-	 * version and link to the store of an Application
-	 */
-	public Application() {
-		this.appName = "";
-		this.description = "";
-		this.platform = "";
-		this.version = "";
-		this.linkToStore = "";
-		this.price = 0;
-		this.comments = new ArrayList<String>();
-	}
-	
-	/*
-	 * Workhorse constructor that receives values for all instance properties 
-	 * and adds an ArrayList parameter to the constructor
-	 * It also initializes the Application object
-	 */
-	public Application(String appName, ArrayList<String> comments) {
-		this.appName = appName;
-		this.comments = comments;
-		this.description = "";
-		this.platform = "";
-		this.version = "";
-		this.linkToStore = "";
-		this.price = 0;
-		
-	}
-	
-	public Application(String appName) {
-		this.appName = appName;
-		this.comments = new ArrayList<String>();
-		this.description = "";
-		this.platform = "";
-		this.version = "";
-		this.linkToStore = "";
-		this.price = 0;
-		this.comments = new ArrayList<String>();
-		
-	}
-	
+    /*	
+     * Default constructor that initializes the name, description, platform, 
+     * version and link to the store of an Application
+     */
+    public Application() {
+        this.appName = "";
+        this.description = "";
+        this.organization = "";
+        this.platform = "";
+        this.version = "";
+        this.linkToStore = "";
+        this.price = 0;
+        this.comments = new ArrayList<String>();
+    }
 
-	public boolean equals(Application a2) {
-		if ((this.appName).equals(a2.appName)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	/*
-	 * compares applications and returns -1 if this app is alphabetically first, 1 if
-	 * a2 is alphabetically first, 0 if this app and a2 are equal (based on appName)
-	 */
-	public int compareTo(Application a2) {
-		if (this.equals(a2)) {
-			return 0;
-		} else if (this.appName.compareTo(a2.appName) > 0) {
-				return 1;
-			}
-		else {
-			return -1;
-		}
-		 
-	}
-	
-	
+    /*
+     * Workhorse constructor that receives values for all instance properties 
+     * and adds an ArrayList parameter to the constructor
+     * It also initializes the Application object
+     */
+    Application(String name, String description, String organization, String platform, String version, String link, float price) {
+        this.appName = name;
+        this.description = description;
+        this.organization = organization;
+        this.platform = platform;
+        this.version = version;
+        this.linkToStore = link;
+        this.price = price;
+    }
+
+    public Application(String appName) {
+        this.appName = appName;
+        this.description = "";
+        this.platform = "";
+        this.version = "";
+        this.linkToStore = "";
+        this.price = 0;
+        this.comments = new ArrayList<String>();
+
+    }
+
+    public boolean equals(Application a2) {
+        if ((this.appName).equals(a2.appName)) {
+                return true;
+        } else {
+                return false;
+        }
+    }
+
+    /*
+     * compares applications and returns -1 if this app is alphabetically first, 1 if
+     * a2 is alphabetically first, 0 if this app and a2 are equal (based on appName)
+     */
+    public int compareTo(Application a2) {
+        if (this.equals(a2)) {
+            return 0;
+        } else if (this.appName.compareTo(a2.appName) > 0) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }	
 }
