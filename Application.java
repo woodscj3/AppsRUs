c0a32825f592febe9d677f3f5df30ea08b495e1c
doantx <92896474+doantx@users.noreply.github.com>
2021-11-16 15:39:54 -0500

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,8 +1,14 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 public class Application {
-    public String appName, description, organization, platform, version, linkToStore; 
-    public float price; 
+    public String appName, description, organization, platform, linkToStore, version;
+    double price;
     public ArrayList<String> comments; 
 
     /*	
@@ -16,7 +22,7 @@ public class Application {
         this.platform = "";
         this.version = "";
         this.linkToStore = "";
-        this.price = 0;
+        this.price = 0.0;
         this.comments = new ArrayList<String>();
     }
 
@@ -25,7 +31,7 @@ public class Application {
      * and adds an ArrayList parameter to the constructor
      * It also initializes the Application object
      */
-    Application(String name, String description, String organization, String platform, String version, String link, float price) {
+    Application(String name, String description, String organization, String platform, String version, String link, double price) {
         this.appName = name;
         this.description = description;
         this.organization = organization;
@@ -35,26 +41,17 @@ public class Application {
         this.price = price;
     }
 
-    /*
-     * Application constructor that receives a String value for the
-     * app's name, then initializes the Application object with that name.
-     */
     public Application(String appName) {
         this.appName = appName;
         this.description = "";
         this.platform = "";
         this.version = "";
         this.linkToStore = "";
-        this.price = 0;
+        this.price = 0.0;
         this.comments = new ArrayList<String>();
 
     }
 
-    /*
-     * Equals method that checks if two Application objects have the same
-     * name. This method receives an Application object against which the
-     * original Application object will be checked.
-     */
     public boolean equals(Application a2) {
         if ((this.appName).equals(a2.appName)) {
                 return true;
@@ -63,8 +60,12 @@ public class Application {
         }
     }
 
+    public String getAppName() {
+        return appName;
+    }
+    
     /*
-     * Compares applications and returns -1 if this app is alphabetically first, 1 if
+     * compares applications and returns -1 if this app is alphabetically first, 1 if
      * a2 is alphabetically first, 0 if this app and a2 are equal (based on appName)
      */
     public int compareTo(Application a2) {
@@ -75,5 +76,53 @@ public class Application {
         } else {
             return -1;
         }
-    }	
+    }
+    
+    /**
+     * Request a new application
+     */
+    public void requestApp(String name) {
+        PreparedStatement ps;
+        String query = "INSERT INTO `AppInfo`(`name`, `description`, `organization`, `platform`, `version`, `link`, `price`) VALUES (?,?,?,?,?,?,?)";
+        
+        try {
+            ps = ConnectData.getConnection().prepareStatement(query);
+            
+            ps.setString(1, name); 
+            ps.setString(2, description);
+            ps.setString(3, organization);
+            ps.setString(4, platform);
+            ps.setString(5, version);
+            ps.setString(6, linkToStore);
+            ps.setDouble(7, price);
+
+            if(ps.executeUpdate() > 0) {
+                JOptionPane.showMessageDialog(null, "App successfully added");
+            } else {
+                JOptionPane.showMessageDialog(null, "Failed");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static Application getAppByName(String input) {
+        Application currApp = null;
+        ConnectData connector = new ConnectData();
+        
+        String query = "SELECT * FROM `AppInfo` WHERE `name` = '" + input + "'";
+        ResultSet rs = connector.getData(query);
+        
+        try {
+            if(rs.next()) {
+                currApp = new Application(rs.getString(1), rs.getString(2), rs.getString(3),
+                        rs.getString(4), rs.getString(5), rs.getString(6), rs.getDouble(7));
+                return currApp;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return currApp;
+    }
 }
