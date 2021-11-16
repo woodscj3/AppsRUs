14d1bc970cd2d8cfd0b305e8521a41d61bb238fc
doantx <92896474+doantx@users.noreply.github.com>
2021-11-16 16:52:12 -0500

    Update Login_JFrame.java

--- Login_JFrame.java
+++ Login_JFrame.java
@@ -1,11 +1,14 @@
 
+import java.awt.Color;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.border.Border;
 
 /**
  *
@@ -18,9 +21,9 @@ public class Login_JFrame extends javax.swing.JFrame {
      */
     public Login_JFrame() {
         initComponents();
-        
-        // Display the JFrame in center
-        this.setLocationRelativeTo(null);
+        this.setLocationRelativeTo(null); //Center
+        Border bd = BorderFactory.createMatteBorder(2, 2, 2, 2, Color.white);
+        jPanel1.setBorder(bd);
     }
 
     /**
@@ -48,6 +51,7 @@ public class Login_JFrame extends javax.swing.JFrame {
         jPasswordField_password = new javax.swing.JPasswordField();
         jTextField_username = new javax.swing.JTextField();
         jLabel_question = new javax.swing.JLabel();
+        jLabel_goToDashboard = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -55,7 +59,7 @@ public class Login_JFrame extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(2, 48, 71));
         jPanel1.setAutoscrolls(true);
 
-        jPanel_portal.setBackground(new java.awt.Color(235, 183, 55));
+        jPanel_portal.setBackground(new java.awt.Color(235, 183, 3));
 
         jLabel1.setBackground(new java.awt.Color(235, 183, 55));
         jLabel1.setFont(new java.awt.Font("Gurmukhi MN", 0, 36)); // NOI18N
@@ -215,34 +219,45 @@ public class Login_JFrame extends javax.swing.JFrame {
         jLabel_question.setForeground(new java.awt.Color(204, 204, 204));
         jLabel_question.setText("Do not have an account?");
 
+        jLabel_goToDashboard.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
+        jLabel_goToDashboard.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel_goToDashboard.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel_goToDashboard.setText("Go to dashboard");
+        jLabel_goToDashboard.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLabel_goToDashboard.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel_goToDashboardMouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(30, 30, 30)
-                        .addComponent(jPanel_login, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(14, 14, 14)
-                        .addComponent(jPanel_portal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(14, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel_minimizeWindow, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel_close, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(25, 25, 25))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel_register)
-                        .addGap(141, 141, 141))))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
                 .addComponent(jLabel_question)
                 .addGap(150, 150, 150))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel_goToDashboard, javax.swing.GroupLayout.PREFERRED_SIZE, 187, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel_register))
+                .addGap(141, 141, 141))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel_minimizeWindow, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel_close, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                            .addGap(30, 30, 30)
+                            .addComponent(jPanel_login, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                            .addGap(14, 14, 14)
+                            .addComponent(jPanel_portal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(14, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -259,7 +274,9 @@ public class Login_JFrame extends javax.swing.JFrame {
                 .addComponent(jLabel_question)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel_register)
-                .addContainerGap(187, Short.MAX_VALUE))
+                .addGap(49, 49, 49)
+                .addComponent(jLabel_goToDashboard)
+                .addContainerGap(116, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -268,8 +285,8 @@ public class Login_JFrame extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -296,7 +313,7 @@ public class Login_JFrame extends javax.swing.JFrame {
         String username = jTextField_username.getText();
         String password = String.valueOf(jPasswordField_password.getPassword());
         
-        String query = "SELECT * FROM `UserInfo` WHERE `username`=? AND `password`=?"; 
+        String query = "SELECT * FROM `UserInfo` WHERE `username`=? AND `userPassword`=?"; 
         
         try {
             ps = ConnectData.getConnection().prepareStatement(query);
@@ -304,10 +321,19 @@ public class Login_JFrame extends javax.swing.JFrame {
             ps.setString(2, password);
             rs = ps.executeQuery();
             
+            // User exists. Login successful
             if(rs.next()) {
-                JOptionPane.showMessageDialog(null, "Login successful");
+                //JOptionPane.showMessageDialog(null, "Login successful");
+                DashboardJFrame dashboard = new DashboardJFrame(); 
+                dashboard.setVisible(true);
+                dashboard.pack();
+                dashboard.setLocationRelativeTo(null);
+                dashboard.jLabel_welcome.setText("WELCOME, " + username + "!");
+                this.dispose();
+            
+            // User does not exist or invalid username/password. Login failed
             } else {
-                JOptionPane.showMessageDialog(null, "Failed login");
+                JOptionPane.showMessageDialog(null, "Invalid username or password.");
             }
             
         } catch (SQLException ex) {
@@ -340,33 +366,19 @@ public class Login_JFrame extends javax.swing.JFrame {
         System.exit(0);
     }                                         
 
+    private void jLabel_goToDashboardMouseClicked(java.awt.event.MouseEvent evt) {                                                  
+        DashboardJFrame dashboard = new DashboardJFrame();
+        dashboard.setVisible(true);
+        dashboard.pack();
+        dashboard.setLocationRelativeTo(null);
+        dashboard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.dispose(); //Terminate login form if dashboard appears
+    }                                                 
+
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login_JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login_JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login_JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login_JFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -381,6 +393,7 @@ public class Login_JFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel_close;
     private javax.swing.JLabel jLabel_displayPass;
+    private javax.swing.JLabel jLabel_goToDashboard;
     private javax.swing.JLabel jLabel_minimizeWindow;
     private javax.swing.JLabel jLabel_pass;
     private javax.swing.JLabel jLabel_question;
