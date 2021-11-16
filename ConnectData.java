5fe7df800bfd9129828ea4e3413b7c590a4f45dd
doantx <92896474+doantx@users.noreply.github.com>
2021-11-16 15:40:33 -0500

    Update ConnectData.java

--- ConnectData.java
+++ ConnectData.java
@@ -1,5 +1,10 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -11,10 +16,25 @@ public class ConnectData {
         Connection con = null;
          try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost/userinfo", "root", "");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/userinfo", "root", "");
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
         return con; 
+    }
+    
+    public ResultSet getData(String input) {
+        PreparedStatement ps;
+        ResultSet rs = null;
+        
+        try {
+            ps = getConnection().prepareStatement(input);
+            rs = ps.executeQuery();
+            return rs;
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return rs;
     }
 }
