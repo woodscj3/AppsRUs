825432014a60d662809565b16ca707e71c181bc7
doantx <92896474+doantx@users.noreply.github.com>
2021-11-30 00:11:35 -0500

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,15 +1,14 @@
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class Application {
-    public String appName, description, organization, platform, linkToStore, version;
+    public String appName, description, organization, platform, linkToStore, version, comment;
     double price;
-    public ArrayList<String> comments; 
+    //public ArrayList<String> comments; 
 
     /*	
      * Default constructor that initializes the name, description, platform, 
@@ -23,12 +22,11 @@ public class Application {
         this.version = "";
         this.linkToStore = "";
         this.price = 0.0;
-        this.comments = new ArrayList<String>();
+        this.comment = "";
     }
 
     /*
      * Workhorse constructor that receives values for all instance properties 
-     * and adds an ArrayList parameter to the constructor
      * It also initializes the Application object
      */
     Application(String name, String description, String organization, String platform, String version, String link, double price) {
@@ -48,7 +46,22 @@ public class Application {
         this.version = "";
         this.linkToStore = "";
         this.price = 0.0;
-        this.comments = new ArrayList<String>();
+        this.comment = "";
+
+    }
+    
+    // A constructor that receives the app name and comment as Strings 
+    // and instantiates comment for the specified app.
+    public Application(String appName, String comment) {
+        this.appName = appName;
+        this.comment = comment;
+    }
+    
+    public Application(String appName, String appDescription, String appOrganization, double appPrice) {
+        this.appName = appName;
+        this.description = appDescription;
+        this.organization = appOrganization;
+        this.price = appPrice;
 
     }
 
@@ -98,16 +111,48 @@ public class Application {
             ps.setDouble(7, price);
 
             if(ps.executeUpdate() > 0) {
-                JOptionPane.showMessageDialog(null, "App successfully added");
+                JOptionPane.showMessageDialog(null, "Successful app request");
             } else {
-                JOptionPane.showMessageDialog(null, "Failed");
+                JOptionPane.showMessageDialog(null, "Failed app request");
             }
         } catch (SQLException ex) {
             Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public static Application getAppByName(String table ,String input) {
+    /**
+     * Add a new app to the database
+     */
+    public void addAppToDB(String name) {
+        PreparedStatement ps;
+        String query = "INSERT INTO `AppInfo`(`appName`, `appDescription`, `appOrganization`, "
+                + "`appPlatform`, `appVersion`, `appLink`, `appPrice`) VALUES (?,?,?,?,?,?,?)";
+        
+        try {
+            ps = ConnectData.getConnection().prepareStatement(query);
+            ps.setString(1, appName); 
+            ps.setString(2, description);
+            ps.setString(3, organization);
+            ps.setString(4, platform);
+            ps.setString(5, version);
+            ps.setString(6, linkToStore);
+            ps.setDouble(7, price);
+            
+            if(ps.executeUpdate() > 0) {
+                JOptionPane.showMessageDialog(null, "Add request approved");
+            } else {
+                JOptionPane.showMessageDialog(null, "Add request denied");
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Return the app that has the corresponding name and that belongs to the appropriate table 
+     */
+    public static Application getAppByName(String table, String input) {
         Application currApp = null;
         ConnectData connector = new ConnectData();
         
@@ -119,6 +164,28 @@ public class Application {
             if(rs.next()) {
                 currApp = new Application(rs.getString(1), rs.getString(2), rs.getString(3),
                         rs.getString(4), rs.getString(5), rs.getString(6), rs.getDouble(7));
+                return currApp;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return currApp;
+    }
+    
+    /**
+     * Return the app that has a comment in the comment table
+     */
+    public static Application getAppComment(String input) {
+        Application currApp = null;
+        ConnectData connector = new ConnectData();
+        
+        String query = "SELECT * FROM `AppComment` WHERE `appName` = '" + input + "'";
+        ResultSet rs = connector.getData(query);
+        
+        try {
+            if(rs.next()) {
+                currApp = new Application(rs.getString(1), rs.getString(2));
                 return currApp;
             }
         } catch (SQLException ex) {
