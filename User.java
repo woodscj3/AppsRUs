8cbf323376fe8be1dbb1a26399b3d66e02858535
doantx <92896474+doantx@users.noreply.github.com>
2021-11-30 00:09:21 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,39 +1,38 @@
-import java.util.ArrayList;
 
 /*
  * A generic representation of a User which contains a username and password
  * */
-
 public class User {
 
-	public String username;
-	public String password;
+    public String username;
+    public String userPassword;
+    public String userType;
 
-	/*
-	 * Default constructor that initializes the name and password as "" and ID as 0
-	 */
-	public User() {
-		this.username = "";
-		this.password = "";
-	}
-	
-	/*
-	 * Workhorse constructor that receives values for all instance properties 
-	 * and initializes the User object
-	 */
-	public User(String username, String password) {
-		this.username = username;
-		this.password = password;
-	}
-	
-	void addComment(Application a, String userComment) {
-		a.comments.add(userComment);
-	}
-	
-	/*
-	 * Takes request form as paramter and adds to requestList parameter
-	 */
-	void submitRequest(RequestForm f, ArrayList<RequestForm> requestList) {
-		requestList.add(f);
-	}
+    /*
+     * Default constructor that initializes the name and password as "" and 
+     * type as normal user
+     */
+    public User() {
+        this.username = "";
+        this.userPassword = "";
+        this.userType = "User";
+    }
+
+    /*
+     * Workhorse constructor that receives values for all instance properties 
+     * and initializes the User object
+     */
+    public User(String username, String password, String type) {
+        this.username = username;
+        this.userPassword = password;
+        this.userType = type;
+    }
+
+//    /*
+//     * Takes request form as paramter and adds to requestList parameter
+//     */
+//    void submitRequest(RequestForm f, ArrayList<RequestForm> requestList) {
+//        requestList.add(f);
+//    }
+    
 }
